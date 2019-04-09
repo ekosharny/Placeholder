5ca8497607f2acca5b0a5f9b555ec3036615b2b9
elonakosharny <ekosharny@gmail.com>
2019-04-09 15:29:17 -0400

    created imagebuttons for the menu

--- app/src/main/java/com/example/placeholder/MainActivity.java
+++ app/src/main/java/com/example/placeholder/MainActivity.java
@@ -1,17 +1,12 @@
 package com.example.placeholder;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.content.Intent;
-
-import android.app.Activity;
-import android.graphics.Color;
-
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
