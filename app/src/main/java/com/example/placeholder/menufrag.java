f1f04a1871ba03494575cd6ead4262a12d8bff23
elonakosharny <ekosharny@gmail.com>
2019-04-09 13:53:18 -0400

    created imagebuttons for the menu

--- app/src/main/java/com/example/placeholder/menufrag.java
+++ app/src/main/java/com/example/placeholder/menufrag.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,9 +22,6 @@ public class menufrag extends Fragment{
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.menu,container,false);
-
-        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
-        transaction.commit();
 
         bowls = view.findViewById(R.id.bowlsButton);
         drinks =  view.findViewById(R.id.drinksButton);
