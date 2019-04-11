51305d7887d80633ecfb43574df5655ba3125ede
elonakosharny <ekosharny@gmail.com>
2019-04-11 16:40:21 -0400

    updated database helper and created customer class

---
+++ app/src/main/java/com/example/placeholder/Customer.java
@@ -0,0 +1,37 @@
+package com.example.placeholder;
+
+public class Customer {
+    private String name;
+    private String email;
+    private String password;
+
+    public Customer(){}
+    public Customer(String b, String c, String d){
+        this.name = b;
+        this.email = c;
+        this.password = d;
+    }
+
+    public void setName(String a){
+        this.name= a;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    public void setEmail(String a){
+        this.email= a;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+    public void setPassword(String a){
+        this.password= a;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
+
+}
