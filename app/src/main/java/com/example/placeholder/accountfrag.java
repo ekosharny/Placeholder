bc718849997e67fc382f4e90703246516765ea16
elonakosharny <ekosharny@gmail.com>
2019-05-12 16:27:57 -0400

    updated accountfrag and settings to work with firebase

--- app/src/main/java/com/example/placeholder/accountfrag.java
+++ app/src/main/java/com/example/placeholder/accountfrag.java
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 //ACCOUNT TAB
 
@@ -14,6 +18,9 @@ public class accountfrag extends AppCompatActivity {
 
     //create variables for the buttons in the layout
     private Button logout;
+    FirebaseAuth user;
+    TextView email;
+    String demail;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,6 +29,7 @@ public class accountfrag extends AppCompatActivity {
 
         //link buttons to id of elements in layout
         logout = findViewById(R.id.logout);
+        email = findViewById(R.id.emailBox);
 
 
         //logout button redirects back to main page
@@ -31,6 +39,24 @@ public class accountfrag extends AppCompatActivity {
                 finish();
             }
         });
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+
+            demail = user.getEmail();
+
+            boolean emailVerified = user.isEmailVerified();
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getIdToken() instead.
+            String uid = user.getUid();
+        }
+
+        email.setText(demail);
+
 
 
     }
--- app/src/main/java/com/example/placeholder/settings.java
+++ app/src/main/java/com/example/placeholder/settings.java
@@ -9,11 +9,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class settings extends AppCompatActivity {
 
     //creates variables for buttons/text-edit
-    Button delete, change, update;
-    EditText confirmUsername, confirmPassword, changeName, changeUsername, changePassword;
+    Button delete, save;
+    EditText changeUsername, changePassword, changePassword2;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,133 +26,52 @@ public class settings extends AppCompatActivity {
 
         //links variable to id in layout
         delete = findViewById(R.id.deletebutton);
-        change = findViewById(R.id.changeButton);
-        update = findViewById(R.id.updateButton);
-        confirmUsername = findViewById(R.id.conUser);
-        confirmPassword = findViewById(R.id.conPass);
-        changeName = findViewById(R.id.changename);
+        save = findViewById(R.id.saveButton);
         changeUsername = findViewById(R.id.changeusername);
         changePassword = findViewById(R.id.changepassword);
+        changePassword2 = findViewById(R.id.changepassword2);
 
+        user = FirebaseAuth.getInstance().getCurrentUser();
 
         //delete button action
         delete.setOnClickListener(new OnClickListener(){
             public void onClick(View v){
-                //creates a new customer object that finds user
-                Customer customer = findUser(v);
-                //if username exists in the database and it matches the password on the record then call deleteAccount method and redirct to main login page
-                if(confirmUsername.getText().toString().equals(customer.getEmail()) && confirmPassword.getText().toString().equals(customer.getPassword())) {
-                    boolean x = deleteAccount(customer);
-                    if(x==true)
-                        Toast.makeText(getApplicationContext(), "Account deleted", Toast.LENGTH_SHORT).show();
-                    else
-                        Toast.makeText(getApplicationContext(), "Couldn't delete account", Toast.LENGTH_SHORT).show();
+                if(user!=null) {
+
+                    user.delete();
 
                     startActivity(new Intent(settings.this, SignIn.class));
                 }
 
-                //display message if username doesn't exist in database or password is wrong
                 else
-                    Toast.makeText(getApplicationContext(), "Wrong username or password", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "You are not logged in", Toast.LENGTH_SHORT).show();
 
             }
         });
 
-        change.setOnClickListener(new OnClickListener() {
+        save.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                //creates a new customer object that finds user
-                Customer customer = findUser(v);
-                if(customer!=null) {
-                    //if username exists in the database and it matches the password on the record then call deleteAccount method and redirct to main login page
-                    if (confirmUsername.getText().toString().equals(customer.getEmail()) && confirmPassword.getText().toString().equals(customer.getPassword())) {
-                        changeName.setVisibility(View.VISIBLE);
-                        changeUsername.setVisibility(View.VISIBLE);
-                        changePassword.setVisibility(View.VISIBLE);
-                        update.setVisibility(View.VISIBLE);
-
+                if(user!=null) {
+                    if(!(changeUsername.getText().toString().equals(""))){
+                        user.updateEmail(changeUsername.getText().toString());
                     }
 
-                    //display message if username doesn't exist in database or password is wrong
-                    else
-                        Toast.makeText(getApplicationContext(), "Wrong username or password", Toast.LENGTH_SHORT).show();
-
+                    if(!changePassword.getText().toString().equals("")){
+                        if(changePassword.getText().toString().equals(changePassword2.getText().toString()))
+                            user.updatePassword(changePassword.getText().toString());
+                        else
+                            Toast.makeText(getApplicationContext(), "Passwords don't match", Toast.LENGTH_SHORT).show();
+                    }
                 }
-                else
-                    Toast.makeText(getApplicationContext(), "Account doesn't exist",
-                            Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Account Updated", Toast.LENGTH_SHORT).show();
+                changeUsername.setText("");
+                changePassword.setText("");
+                changePassword2.setText("");
+
             }
         });
 
-        update.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Customer customer = findUser(v);
-                boolean x = updateCustomer(customer);
-                if(x==true)
-                    Toast.makeText(getApplicationContext(), "Account Updated", Toast.LENGTH_SHORT).show();
-                else
-                    Toast.makeText(getApplicationContext(), "Couldn't Update Account", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-    //delete method deletes a customer from the database
-    public boolean deleteAccount(Customer customer){
-        //creates new DatabaseHelper and calls deleteHandler
-        DatabaseHelper dhelper = new DatabaseHelper(this);
-        boolean x = dhelper.deleteHandler(customer.getName());
-        return x;
-    }
-
-    //uses username input to find the user in the database
-    public Customer findUser(View v){
-        //creates a new DatabaseHelper object
-        DatabaseHelper dhelper = new DatabaseHelper(this);
-
-        //creates a new customer and uses findHandler method in DatabaseHelper to find user
-        Customer customer =  dhelper.findHandler(confirmUsername.getText().toString());
-        //returns the customer found in database if not null
-        if(customer!=null){
-            return customer;
-        }
-        else
-            return null;
-    }
-
-    //this method updates whatever the uses changes
-    //the if statement tests for different possibilities of input
-    public boolean updateCustomer(Customer customer){
-        DatabaseHelper dhelper = new DatabaseHelper(this);
-
-        boolean x=false;
-
-        if(!(changeName.getText().toString().equals("")) && !(changeUsername.getText().toString().equals("")) && !(changePassword.getText().toString().equals(""))){
-            x = dhelper.updateHandler(changeName.getText().toString(), changeUsername.getText().toString(), changePassword.getText().toString());
-        }
-        else if(!(changeName.getText().toString().equals("")) && (changeUsername.getText().toString().equals("")) && (changePassword.getText().toString().equals(""))){
-            x = dhelper.updateHandler(changeName.getText().toString(), customer.getEmail(), customer.getPassword());
-        }
-        else if(!(changeName.getText().toString().equals("")) && !(changeUsername.getText().toString().equals("")) && (changePassword.getText().toString().equals(""))){
-            x = dhelper.updateHandler(changeName.getText().toString(), changeUsername.getText().toString(), customer.getPassword());
-        }
-        else if(!(changeName.getText().toString().equals("")) && (changeUsername.getText().toString().equals("")) && !(changePassword.getText().toString().equals(""))){
-            x = dhelper.updateHandler(changeName.getText().toString(), customer.getEmail(), changePassword.getText().toString());
-        }
-        else if((changeName.getText().toString().equals("")) && !(changeUsername.getText().toString().equals("")) && !(changePassword.getText().toString().equals(""))){
-            x = dhelper.updateHandler(customer.getName(), changeUsername.getText().toString(), changePassword.getText().toString());
-        }
-        else if((changeName.getText().toString().equals("")) && !(changeUsername.getText().toString().equals("")) && (changePassword.getText().toString().equals(""))){
-            x = dhelper.updateHandler(customer.getName(), changeUsername.getText().toString(), customer.getPassword());
-        }
-        else if((changeName.getText().toString().equals("")) && (changeUsername.getText().toString().equals("")) && !(changePassword.getText().toString().equals(""))){
-            x = dhelper.updateHandler(customer.getName(), customer.getEmail().toString(), changePassword.getText().toString());
-        }
-        else if((changeName.getText().toString().equals("")) && (changeUsername.getText().toString().equals("")) && (changePassword.getText().toString().equals(""))){
-            x = dhelper.updateHandler(customer.getName(), customer.getEmail(), customer.getPassword());
-        }
-
-        return x;
     }
 
 }
