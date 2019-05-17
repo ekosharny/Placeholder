c93e76866b52c3236f05a11a182e9854539cfea0
elonakosharny <ekosharny@gmail.com>
2019-05-17 12:33:02 -0400

    added premade bowls layout

---
+++ app/src/main/java/com/example/placeholder/PremadeBowls.java
@@ -0,0 +1,13 @@
+package com.example.placeholder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PremadeBowls extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_premade_bowls);
+    }
+}
