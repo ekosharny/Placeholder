d6b6c0f4663c7132913ffef4d11577397caa6cbf
elonakosharny <ekosharny@gmail.com>
2019-05-17 15:49:23 -0400

    updates

--- app/src/main/java/com/example/placeholder/PremadeBowls.java
+++ app/src/main/java/com/example/placeholder/PremadeBowls.java
@@ -1,13 +1,18 @@
 package com.example.placeholder;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.view.View.OnClickListener;
 
 public class PremadeBowls extends AppCompatActivity {
 
     Button calib, tritrib, macfergusb, hawaiianb, vegeterianb, farmerb;
     String cali, tritri, macfergus, hawaiian, vegeterian, farmer;
+
+    Button create;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +34,15 @@ public class PremadeBowls extends AppCompatActivity {
         hawaiian=findViewById(R.id.hawaiianBowl).toString();
         vegeterian =findViewById(R.id.vegetarianBowl).toString();
         farmer=findViewById(R.id.farmerBowl).toString();
+
+        create= findViewById(R.id.createOwnBowlButton);
+
+        create.setOnClickListener(new OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(PremadeBowls.this, CreateBowls.class));
+            }
+        });
+
+
     }
 }
