a8cadb71458b8b248b6c24b0594c31c5dddb0ae2
Tristen Pellot <tristenpellot22@gmail.com>
2019-05-19 18:04:22 -0400

    returnToMenu button redirects to mainactivity when clicked

--- app/src/main/java/com/example/placeholder/CheckoutComplete.java
+++ app/src/main/java/com/example/placeholder/CheckoutComplete.java
@@ -1,13 +1,28 @@
 package com.example.placeholder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CheckoutComplete extends AppCompatActivity {
+
+    Button returnToMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout_complete);
+
+        returnToMenu = findViewById(R.id.returnToMenuButton);
+
+        returnToMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CheckoutComplete.this, MainActivity.class));
+                finish();
+            }
+        });
     }
 }
