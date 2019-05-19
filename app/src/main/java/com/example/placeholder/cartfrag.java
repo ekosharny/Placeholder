d951880814e9543f565b66d4cb7657f45db1b176
elonakosharny <ekosharny@gmail.com>
2019-05-19 13:55:35 -0400

    shopping cart update

--- app/src/main/java/com/example/placeholder/cartfrag.java
+++ app/src/main/java/com/example/placeholder/cartfrag.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class cartfrag extends Fragment{
 
@@ -20,12 +21,38 @@ public class cartfrag extends Fragment{
 
         //checkOutItems=view.findViewById(R.id.itemsTV);
 
+        items = view.findViewById(R.id.items);
+        prices = view.findViewById(R.id.prices);
+        clear = view.findViewById(R.id.clear);
+        checkout = view.findViewById(R.id.checkout);
 
-        //DatabaseHelper dbHandler = new DatabaseHelper(getActivity());
-        //checkOutItems.setText(dbHandler.loadDetails());
+
+        DatabaseHelper dbHandler = new DatabaseHelper(getActivity());
+        items.setText(dbHandler.loadItems());
+        prices.setText(dbHandler.loadPrices());
+
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean x = deleteItems();
+                if(x=true){
+                    items.setText("");
+                    prices.setText("");
+                }
+                else
+                    Toast.makeText(getActivity(), "Error", Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
         return view;
     }
 
+    public boolean deleteItems(){
+        //creates new DatabaseHelper and calls deleteHandler
+        DatabaseHelper dhelper = new DatabaseHelper(getActivity());
+        boolean x = dhelper.deleteDetails(1);
+        return x;
+    }
 
 }
